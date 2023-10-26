12ae505f3e8ff6e3d416db5360ec72a42dcd15aa
Samuel <samuel.okafor@decagon.dev>
2023-10-26 15:06:23 +0100

    update

--- src/main/java/com/example/thymeleafcrudeapplication/Controller/UsersController.java
+++ src/main/java/com/example/thymeleafcrudeapplication/Controller/UsersController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 import java.util.List;
@@ -29,8 +30,9 @@ public class UsersController {
         return "user_form";
     }
     @PostMapping("/users/save")
-    public String saveUser(Users users){
+    public String saveUser(Users users, RedirectAttributes ra){
         usersService.save(users);
+        ra.addFlashAttribute("message","The user has been saved successfully!");
         return "redirect:/users";
     }
 }
