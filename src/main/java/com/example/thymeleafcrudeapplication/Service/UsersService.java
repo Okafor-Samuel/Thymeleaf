135ceb1f0cb6e2fae539369a8f19822330962dfb
Samuel <samuel.okafor@decagon.dev>
2023-10-26 15:12:50 +0100

    update

--- src/main/java/com/example/thymeleafcrudeapplication/Service/UsersService.java
+++ src/main/java/com/example/thymeleafcrudeapplication/Service/UsersService.java
@@ -1,11 +1,13 @@
 package com.example.thymeleafcrudeapplication.Service;
 
 import com.example.thymeleafcrudeapplication.Entity.Users;
+import com.example.thymeleafcrudeapplication.Exception.UserNotFoundException;
 import com.example.thymeleafcrudeapplication.Repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +20,12 @@ public class UsersService {
 
     public void save(Users users) {
         usersRepository.save(users);
+    }
+    public Users get(Long id) throws UserNotFoundException {
+       Optional<Users> targetUser = usersRepository.findById(id);
+        if(targetUser.isPresent()){
+            return targetUser.get();
+        }
+        throw new UserNotFoundException("Could not find any user with id: " +id);
     }
 }
